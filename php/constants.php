85df0c2c7c0b2d2d3d1760ae36ac2ef2ab26dd6d
Alexandre-github-code <alexandre.auguste@isen-ouest.yncrea.fr>
2024-12-05 11:58:02 +0100

    suppression ajax (obsolète), création db / api rest

---
+++ php/constants.php
@@ -0,0 +1,8 @@
+<?php
+  // Database constants.
+  define('DB_USER', 'postgres');
+  define('DB_PASSWORD', 'new password');
+  define('DB_NAME', 'banque');
+  define('DB_SERVER', 'localhost');
+  define('DB_PORT', '5432');
+?>
